abf2a9cfa0306f304e2f118a710fde7c3793e2dd
valeryvanegas-uba <valery02vanegas@gmail.com>
2026-08-28 12:38:22 -0300

    hello world

---
+++ scripts/01 datos.R
@@ -0,0 +1,21 @@
+getwd()
+setwd("C:/Users/admin/Desktop/CDD_Grupo-_UBA/datos")
+
+install.packages(tidyverse)
+
+install.packages(c("arrow", "readr", "dplyr", "tidyr", "ggplot2", 
+                   "janitor", "skimr", "fixest", "modelsummary"))
+install.packages("tidyverse")
+install.packages("haven")
+install.packages("ggrepel")
+
+library(arrow)
+library(tidyverse)
+
+industry_panel <- read_parquet("country_industry_panel.parquet")
+occupation_panel <- read_parquet("country_occupation_panel.parquet")
+task_labels <- read_parquet("task_country_labels_analysis.parquet")
+
+str(industry_panel)
+str(task_labels)
+str(occupation_panel)
